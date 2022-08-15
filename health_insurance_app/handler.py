5c3a096d7f441d467a23efa8fe730e67729134b5
laurivan <laurivansareta@gmail.com>
2022-08-15 17:24:18 -0300

    - Adicionado arquivos para deploy de API REST do modelo em produção. OBS: Ainda faltam alguns ajustes para finalizar e funcionar totalmente.

---
+++ health_insurance_app/handler.py
@@ -0,0 +1,46 @@
+import datetime
+import pickle
+import pandas as pd
+import os
+from flask import Flask, request, Response
+from healthinsurance import HealthInsurance
+
+print(os.getcwd())
+
+# Carragando modelo
+model = pickle.load(open('models/model_linear_regression.pkl', 'rb'))
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/status', methods=['GET'])
+def status():
+    return Response('Api HealthInsurance - Status Online {}'.format(datetime.datetime.today()), status=200, mimetype='application/json')
+
+@app.route('/predict', methods=['POST'])
+def health_insurance_predict():   
+    test_json = request.get_json()
+
+    print(test_json)
+
+    if not test_json:
+        return Response('{}', status=200, mimetype='application/json')
+
+    if isinstance(test_json, dict):
+        teste_raw = pd.DataFrame(test_json, index=[0])
+    else:
+        teste_raw = pd.DataFrame(test_json, index=test_json[0].keys())
+
+    pipeline = HealthInsurance()
+    df = pipeline.data_cleaning(teste_raw)
+    df = pipeline.feature_engineering(df)
+    df = pipeline.data_preparation(df)
+
+    return pipeline.get_prediction(model, teste_raw, df)
+
+if __name__ == '__main__':
+    # Heroku
+    # port = os.environ.get('PORT', 5000)
+    # app.run('0.0.0.0', port=port)
+    # Local
+    app.run('0.0.0.0', debug=True)
---
+++ health_insurance_app/healthinsurance/HealthInsurance.py
@@ -0,0 +1,78 @@
+import pickle
+import numpy as np
+import pandas as pd
+
+class HealthInsurance( object ):
+    def __init__( self ):
+        print('__init__')
+        self.home_path = '..'
+
+        # Standardization
+        self.annual_premium_scaler = pickle.load(open(self.home_path + 'features/annual_premium_scaler.pkl', 'rb'))
+        # Rescaling
+        self.age_scaler = pickle.load(open(self.home_path + 'features/age_scaler.pkl', 'rb'))
+        self.vintage_scaler = pickle.load(open(self.home_path + 'features/vintage_scaler.pkl', 'rb'))
+        # Encoder
+        self.target_encode_gender_scaler = pickle.load(open(self.home_path + 'features/target_encode_gender_scaler.pkl', 'rb'))
+        self.target_encode_region_code_scaler = pickle.load(open(self.home_path + 'features/target_encode_region_code_scaler.pkl', 'rb'))
+        self.te_policy_sales_chanel_scaler = pickle.load(open(self.home_path + 'features/te_policy_sales_chanel_scaler.pkl', 'rb'))
+        
+    def data_cleaning( self, df1 ):
+        print('data_cleaning')
+        cols_new = ['id', 'gender', 'age', 'driving_license', 'region_code',
+                    'previously_insured', 'vehicle_age', 'vehicle_damage', 'annual_premium',
+                    'policy_sales_channel', 'vintage', 'response']
+        df1.columns = cols_new
+        
+        return df1
+        
+    def feature_engineering( self, df2):
+        print('feature_engineering')
+
+        # Vehicle age
+        df2['vehicle_age'] = df2['vehicle_age'].apply(lambda x: 'over_2_years' if x=='> 2 Years' else 'between_1_2_year' 
+                                                        if x=='1-2 Year' else 'below_1_year')
+
+        # vehicle damage
+        df2['vehicle_damage'] = df2['vehicle_damage'].apply(lambda x: 1 if x=='Yes' else 0)
+
+        return df2
+        
+    def data_preparation( self, df3 ):
+        print('data_preparation')
+
+        # annual_premium
+        df5['annual_premium'] = self.annual_premium_scaler.transform(df5[['annual_premium']].values)
+
+        # Age
+        df5['age'] = self.age_scaler.transform(df5[['age']].values)
+
+        # vintage
+        df5['vintage'] = self.vintage_scaler.transform(df5[['vintage']].values)
+
+        # gender -> One Hot Encoding / Frequency Encoding        
+        df5.loc[:, 'gender'] = df5['gender'].map(self.target_encode_gender_scaler)
+
+        # region_code -> Target Encoding / Frequency Encoding / Weighted Target Encoding        
+        df5.loc[:, 'region_code'] = df5['region_code'].map(self.target_encode_gender_scaler)
+
+        # vehicle_age -> One Hot Encoding / Order Encoding / Frequency Encoding 
+        df5 = pd.get_dummies(df5, prefix='vehicle_age', columns=['vehicle_age'])
+
+        # policy_sales_channel - > Frequency Encoding / Target Encoding
+        df5.loc[:, 'policy_sales_channel'] = df5['policy_sales_channel'].map(self.te_policy_sales_chanel_scaler)
+
+        cols_selected = ['annual_premium', 'vintage', 'age', 'region_code', 'vehicle_damage', 'previously_insured',
+                 'policy_sales_channel']
+
+        return df5[cols_selected]
+        
+    def get_prediction(self, model, original_data, test_data):
+        print('get_prediction')
+        # Model
+        pred = model.predict_proba(test_data)
+
+        # Une a predição com dados originais
+        original_data['score'] = pred
+
+        return original_data.to_json(orient='records', date_format='iso')
